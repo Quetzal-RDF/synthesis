22ce8faf6ad101897dfc7412d8cfff7ad1c9c35d
Julian Dolby <juliandolby@gmail.com>
2017-07-25 12:48:06 -0400

    new embeddings

--- parse.rkt
+++ parse.rkt
@@ -2,22 +2,43 @@
 
 (define (make-parser . column-names)
   (let ((keywords
-         '((or ("or") ("either") ("any") ("instead"))
-           (and ("and") ("both") ("also") ("including"))
-           (not ("not") ("never") ("neither") ("no") ("don't") ("isn't"))
-           (concatenate ("concatenate") ("combine") ("transform") ("append") ("merge") ("mix") ("encode"))
-           (quotient ("quotient"))
-           (remainder ("remainder")("modulo")("mod")("%"))
-           (abs ("abs") ("absolute") ("magnitude") ("positive"))
-           (ceiling ("ceiling") ("round" "up"))
-           (floor ("floor") ("round" "down"))
-           (truncate ("truncate")("trunc")("drop") ("shorten") ("abbreviate") ("cut"))
-           (sign ("sign") ("positive" "negative"))
-           (if ("if") ("whether")("when")("unless"))
-           (substring ("substring")("extract" "characters")("left")("right")("mid"))
-           (index-of ("indexof")("index" "of")("find")("search"))
-           (length ("length")("string" "length")("len")))))
-
+	 '(
+	   (or ("or") ("either") ("any") ("instead"))
+	   (and ("and") ("both") ("also") ("including"))
+	   (not ("not") ("never") ("neither") ("no") ("dont") ("isnt"))
+	   (concatenate ("concatenate") ("concat") ("combine") ("transform") ("append") ("merge") ("mix") ("encode"))
+	   (quotient ("quotient"))
+	   (remainder ("remainder")("modulo")("mod")("%"))
+	   (abs ("abs") ("absolute") ("magnitude") ("positive"))
+	   (ceiling ("ceiling") ("round" "up"))
+	   (floor ("floor") ("round" "down"))
+	   (truncate ("truncate")("trunc")("drop") ("shorten") ("abbreviate") ("cut"))
+	   (sign ("sign") ("positive" "or" "negative"))
+	   (if ("if") ("whether")("when")("unless")("case")("in case")("case" "when"))
+	   (then ("then") ("do") ("then" "do") ("subsquently"))
+	   (else ("else") ("or" "else") ("or") ("otherwise"))
+	   (substring ("substring")("extract" "characters")("left")("right")("mid"))
+	   (index-of ("indexof")("index" "of")("find")("search"))
+	   (length ("length")("string" "length")("len"))
+	   (+ ("+") ("plus"))
+	   (- ("-") ("minus") ("takeaway") ("subtract") ("deduct"))
+	   (/ ("/") ("divide") ("divided by"))
+	   (exponent ("exponent") ("exp") ("power"))
+	   (logarithm ("logarithm") ("log") ("ln") ("natural logarithm"))
+	   (sqrt ("sqrt") ("square" "root") ("square" "root" "of"))
+	   (lower ("lower") ("to" "lower" "case") ("to" "lower") ("convert" "to" "lower" "case"))
+	   (upper ("upper") ("to" "upper" "case") ("to" "upper") ("convert" "to" "upper" "case"))
+	   (trim ("trim") ("remove" "trailing" "spaces") ("pare") ("cut"))
+	   (replace ("replace") ("overlay") ("change"))
+	   (matches ("matches") ("contains") ("includes") ("has") ("appears in"))
+	   (now ("now") ("current" "time") ("today"))
+	   (extract ("extract") ("retrieve") ("pull" "out") ("grab") ("fetch") ("find") ("obtain") ("get"))
+	   (> (">") ("greater") ("greater" "than") ("more") ("more" "than") ("larger") ("larger" "than") ("higher") ("higher" "than") ("bigger") ("bigger" "than") ("older") ("older" "than"))
+	   (< ("<") ("less") ("less" "than") ("smaller") ("smaller" "than") ("lower") ("lower" "than") ("younger") ("younger" "than"))
+	   (>= (">=") ("greater" "than" "or" "equal" "to") ("more" "than" "or" "equal" "to") ("larger" "than" "or" "equal" "to") ("higher" "than" "or" "equal" "to") ("bigger" "than" "or" "equal" "to") ("older" "than" "or" "equal" "to"))
+	   (<= ("<=") ("less" "than" "or" "equal" "to") ("smaller" "than" "or" "equal" "to") ("lower" "than" "or" "equal" "to") ("younger" "than" "or" "equal" "to"))
+	   (= ("=") ("==") ("equals") ("is") ("is equal to")))))
+    
     (define (find-f pred-f val-f lst)
       (if (null? lst)
           #f
@@ -72,7 +93,7 @@
               (#t
                (cons #f tokens)))))
 
-    (define parse-unary-op (parse-op '("-" not)))
+    (define parse-unary-op (parse-op '(- not)))
 
     (define (parse-unary-expr tokens)
       (let* ([unary-op (parse-unary-op tokens)]
@@ -87,7 +108,7 @@
                   x)))))
     
     (define parse-binary-op
-      (parse-op '("=" "+" "plus" "*" "times" "-" "minus" "/" "divide")))
+      (parse-op '(= + "*" "times" '- '/)))
     
     (define (parse-binary-stuff parse-inner-expr parse-operator)
       (lambda (tokens)
@@ -126,13 +147,15 @@
         (if (eq? (car x) 'if)
             (let ((test (parse-andor-expr (cdr x))))
               (if (not (eq? #f (car test)))
-                  (if (equal? (cadr test) "then")
-                      (let ((then (parse-andor-expr (cddr test))))
-                        (if (eq? #f (car then))
-                            (cons (list 'if (car test)) (cdr test))
-                            (cons (list 'if (car test) (car then)) (cdr then))))
-                      (cons (list 'if (car test)) (cdr test)))
-                  (cons (list 'if (car test)) (cdr test))))
+		  (let ((then-test (parse-keyword 'then (cdr test))))
+		    (if (not (eq? #f (car then-test)))
+			(let ((then (parse-andor-expr (cdr then-test))))
+			  (if (eq? #f (car then))
+			      (cons (list 'if (car test)) (cdr test))
+                            
+			      (cons (list 'if (car test) (car then)) (cdr then))))
+			(cons (list 'if (car test)) (cdr test))))
+		  (cons (list 'if (car test)) (cdr test))))
             (parse-andor-expr tokens))))
     
     (define (parse-loop tokens)
@@ -146,3 +169,5 @@
                     (cons (car tokens) (parse-loop (cdr tokens)))))))
 
     parse-loop))
+
+(provide make-parser)
